0f2d511c6da0de647655da928c8b5ad99a7f6f85
bilal-wani <60865104+bilal-wani@users.noreply.github.com>
2025-08-16 17:58:13 +0000

    fixing cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+// Allow requests from your frontend URL
+// app.use(cors({
+//   origin: "https://refactored-capybara-pw56x4w99x6295r-3000.app.github.dev",
+//   methods: ["GET", "POST"],   // add PUT/DELETE if you need them
+//   credentials: true
+// }));
+
 
 app.get("/", (req, res) => {
   res.send("Hello from backend!");
@@ -11,4 +22,4 @@ app.get("/api/hello", (req, res) => {
 
 
 const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT} `));
