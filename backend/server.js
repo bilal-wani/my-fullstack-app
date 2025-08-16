e99d334c3525d0301d4edef8beeac9f8fe7e711d
bilal-wani <60865104+bilal-wani@users.noreply.github.com>
2025-08-16 17:09:56 +0000

    api/hello implemented fe & be

--- backend/server.js
+++ backend/server.js
@@ -5,5 +5,10 @@ app.get("/", (req, res) => {
   res.send("Hello from backend!");
 });
 
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "api/hello API Call" });
+});
+
+
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <h1>Hello from frontend!</h1>;
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    fetch("https://my-backend-bpj3.onrender.com/api/hello")
+      .then(res => res.json())
+      .then(json => setData(json.message));
+  }, []);
+
+  return <h1>Bilal: {data}</h1>;
 }
+
 export default App;
