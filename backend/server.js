d1a2617283fd98a81d5a9f88397a19b07bdcb203
bilal-wani <60865104+bilal-wani@users.noreply.github.com>
2025-08-16 18:12:32 +0000

    api products

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,16 @@ app.get("/api/hello", (req, res) => {
   res.json({ message: "api/hello API Call" });
 });
 
+app.get("/api/products", (req, res) => {
+  const products = [
+    { id: 1, name: "Laptop", price: 75000 },
+    { id: 2, name: "Phone", price: 30000 },
+    { id: 3, name: "Headphones", price: 2000 },
+  ];
+  res.json(products);
+});
+
+
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} `));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,44 @@
 import { useEffect, useState } from "react";
 
+// const deployment = process.env.NODE_ENV === "development";
+const development = false; // Assuming you want to set this to true for local development
+
+  const serverURL = development
+    ? "https://refactored-capybara-pw56x4w99x6295r-5000.app.github.dev"
+    : "https://my-backend-bpj3.onrender.com";
+
 function App() {
-  const [data, setData] = useState("");
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("https://my-backend-bpj3.onrender.com/api/hello")
+    fetch(`${serverURL}/api/products`) // notice no full URL, proxy handles it
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {products.map((p) => (
+          <li key={p.id}>
+            {p.name} - ₹{p.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function AppOld() {
+  const [data, setData] = useState("");
+
+
+
+  useEffect(() => {
+    // fetch("https://my-backend-bpj3.onrender.com/api/hello")
+    fetch(`${serverURL}/api/hello`)
       .then(res => res.json())
       .then(json => setData(json.message));
   }, []);
